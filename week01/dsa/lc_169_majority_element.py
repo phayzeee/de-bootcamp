1b55bfd0049a7f12f7ea02037f84a382522551ad
phayzeee <faizanhmd3@gmail.com>
2026-07-27 19:46:07 +0500

    day 03 dsa done

--- week01/dsa/lc_169_majority_element.py
+++ week01/dsa/lc_169_majority_element.py
@@ -1,14 +1,22 @@
-# from typing import List
-#
-#
-# def majorityElement(nums: List[int]) :
-#     freq = {}
-#
-#     for i in nums:
-#         freq[i] = freq.get(nums[i], 0) + 1
-#
-#     for index,char in enumerate(nums):
-#
-#     print(freq)
-#
-# majorityElement([2,2,1,1,1,2,2])
+from typing import List
+
+
+def majorityElement(self, nums: List[int]) -> int:
+    freq = {}
+    count = 0
+    largestKey = ""
+
+    for i in nums:
+        freq[i] = freq.get(i, 0) + 1
+
+    for key, value in freq.items():
+        if value > count:
+            count = value
+            largestKey = key
+
+    #uppar wale ki jaga ye line : max(freq, key= freq.get) bhi use hosakti h
+
+    return largestKey
+
+
+majorityElement([2,2,1,1,1,2,2])
