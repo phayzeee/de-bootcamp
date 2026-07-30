e13e57a39b7cda6173b54fc829ca6e541a3b6f29
phayzeee <faizanhmd3@gmail.com>
2026-07-30 18:44:07 +0500

    day 05 done

--- week01/dsa/lc01_two_sum.py
+++ week01/dsa/lc01_two_sum.py
@@ -17,6 +17,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         result = {}
 
+
         for i, v in enumerate(nums):
             compliment = target - v
 
